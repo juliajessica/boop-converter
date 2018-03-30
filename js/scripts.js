21324b3b7811920baba899360b6d2e4187a38014
julia sheremet <juliajessicasher@gmail.com>
2018-03-30 08:44:33 -0700

    spec two met: program returns Boop for input of 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 ///backend logic
 var number = [];
+var one = "Beep!"
+var two = "Boop!"
+var three = "I'm sorry, Dave. I'm afraid I can't do that."
+
 function beepboop(number) {
+  // for(i)
   return number;
 }
 
