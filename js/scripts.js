a5680b02d1a3a749c92d9309104f1b640cd6d03d
julia sheremet <juliajessicasher@gmail.com>
2018-03-30 16:27:20 -0700

    finalResult variable should print result

--- js/scripts.js
+++ js/scripts.js
@@ -6,45 +6,51 @@
 
 
 
-function beepBoop(userInput) {
-  var numberDigit = userInput.toString();
-  // var numberDigit = parseInt(userInput);
-  var outputNumber = [];
-
-  for (var i=0;i<=numberDigit.length;i++) {
-
-
-    if (numberDigit[i] === 0) { //true //if it contains a 0
-      numberDigit = numberDigit.replace(numberDigit[i], "Beep!");
-      // outputNumber.push("Beep!");
-      numberDigit.push("Beep!");
-      debugger;
-    }
-  }
-} beepBoop(20);
-//////////////////////////////
 // function beepBoop(userInput) {
 //   var numberDigit = userInput.toString();
+//   // var numberDigit = parseInt(userInput);
 //   var outputNumber = [];
 //
 //   for (var i=0;i<=numberDigit.length;i++) {
-//     var numberDigit = parseInt(userInput);
 //
-//     if (numberDigit[i] === 0) { //true //if it contains a 0
-//     //   number = number.replace(number[i], "Beep!");
-//     //   debugger; }
 //
-//       // return numberDigit + "beep";
-//       // numberDigit = [i].toString().replace(numberDigit[i], "Beep!");
-//       // numberDigit.replace(numberDigit[i], "Beep!");
-//       outputNumber.push("Beep!");
-//       return userName;
-//       // debugger;
-//       // console.log(numberDigit);
-//      }
-//
+//     if (numberDigit[i] === 0) {
+//       numberDigit = numberDigit.replace(numberDigit[i], "Beep!");
+//       // outputNumber.push("Beep!");
+//       numberDigit.push("Beep!");
+//       debugger;
 //     }
+//   }
 // }
+// } beepBoop(20);
+//////////////////////////////
+function beepBoop(number) {
+
+  var numberDigit = number.toString();
+  var outputNumber = [];
+
+  for (var i=0;i<=numberDigit.length;i++) {
+    // var numberDigit = parseInt(numberDigit);
+    debugger;
+    console.log(numberDigit);
+
+    if (numberDigit[i] === 0) {
+      numberDigit.replace(numberDigit[i], "Beep!");  // number = number.replace(number[i], "Beep!");
+      }  else if (number[i] === 1) {
+        numberDigit.replace(numberDigit[i], "Boop!");
+      } else if (numberDigit[i] % 3 == 0) {
+        numberDigit.replace(numberDigit[i], "I'm sorry, Dave. I'm afraid I can't do that.");
+      }
+      // } else {
+      //   return numberDigit;
+      // }
+  }
+  // numberDigit.push("Beep!");
+  return numberDigit;
+  return userName;
+}
+      // return numberDigit + "beep";   // numberDigit = [i].toString().replace(numberDigit[i], "Beep!");
+
 
 /////////////////////////////
   // }   beepBoop(20);
@@ -53,29 +59,21 @@ function beepBoop(userInput) {
   //   number = number.replace(number[i], "Beep!");
   //   debugger; }
 //       console.log(number);
-//     } else if (parseInt(number[i]) === 1) {
-//       number = number.replace(number[i], "Boop!");
-//     } else if (parseInt(number[i]) % 3 == 0) {
-//       number = number.replace(number[i], "I'm sorry, Dave. I'm afraid I can't do that.");
-//       console.log(number);
-//     } return number;
-//       return userName;
-//   }
-// }
+
 
 // user interface
-$(document).ready(function(){
-  $("form#beep-form").submit(function(event){
-    event.preventDefault();
-    debugger;
-
-    var userInput = $("#userInput").val();
-    var userName = $("input#name").val();
-    var finalOutput = beepBoop(userInput);
-
-    $("#userOutput").text(finalOutput);
-    $(".user-name").text(userName);
-    $("#output").show();
-
-  });
-});
+// $(document).ready(function(){
+//   $("form#beep-form").submit(function(event){
+//     event.preventDefault();
+//     debugger;
+//
+//     var userName = $("input#name").val();
+//     var userInput = $("#userInput").val();
+//     var finalOutput = beepBoop(userInput);
+//
+//     $("#userOutput").text(finalOutput);
+//     $(".user-name").text(userName);
+//     $("#output").show();
+//
+//   });
+// });
