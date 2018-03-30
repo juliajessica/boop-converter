30cd4491c78263ad2bec11f4569e29826dd60575
julia sheremet <juliajessicasher@gmail.com>
2018-03-30 09:02:03 -0700

    add for loop and run tests in console

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 ///backend logic
-var number = [];
-var one = "Beep!"
-var two = "Boop!"
+var numberArray = [];
+var zero = "Beep!"
+var one = "Boop!"
 var three = "I'm sorry, Dave. I'm afraid I can't do that."
+var numberToString = numberArray.toString();
 
 function beepboop(number) {
-  for(i=0; i<= number.length)
+  for(i=0;i<=number.length;i++) {
+    if(number === zero) {
+      return numberArray + "Beep!";
+    }
+  }
+
+  // for(i)
+
   return number;
 }
 
